cbdf6b7181417ca5fd86f87686a8a08e8209a717
Luigi Marson Grandi <89944940+Luigi-Grandi@users.noreply.github.com>
2025-07-03 19:55:44 +0000

    finalizacao da pagina status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,29 @@
+import { Console } from "console";
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const resultado = await database.query("SELECT 1 + 1 as SUM;");
-  console.log(resultado.rows);
-  response.status(200).json({ chave: "Teste" });
+  const databaseName = process.env.POSTGRES_DB;
+
+  const resultadoConstConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity where datname = $1;",
+    values: [databaseName],
+  });
+  const resultadoMaxConnections = await database.query("SHOW max_connections;");
+
+  const updatedAt = new Date().toISOString();
+  const resultadoVersion = await database.query("SHOW server_version;");
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: resultadoVersion.rows[0].server_version,
+        max_connections: parseInt(
+          resultadoMaxConnections.rows[0].max_connections,
+        ),
+        used_connections: parseInt(resultadoConstConnections.rows[0].count),
+      },
+    },
+  });
 }
 export default status;
--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,4 +1,16 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const receveidDate = new Date(responseBody.updated_at).toISOString();
+  expect(receveidDate).toEqual(responseBody.updated_at);
+
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+  expect(
+    responseBody.dependencies.database.used_connections,
+  ).toBeGreaterThanOrEqual(1);
+  //expect(!isNaN(responseBody.dependencies.database.max_connections)).toBe(true);
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
 });
